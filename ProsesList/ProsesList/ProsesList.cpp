a53ea698cccfa6b4ddd8e04665c6ba626e194ef5
Muhammet Karul <64870233+Karuulme@users.noreply.github.com>
2022-03-28 10:44:03 +0300

    my first app on msdn
    my first app on msdn

---
+++ ProsesList/ProsesList/ProsesList.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+#include <tlhelp32.h>
+#include <tchar.h>
+
+using namespace std;
+void ProcessList();
+int main()
+{
+	ProcessList();
+}
+void ProcessList() {
+	HANDLE hProcessShot;
+	PROCESSENTRY32 ProcessInformation;
+	hProcessShot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	ProcessInformation.dwSize = sizeof(PROCESSENTRY32);
+	if (Process32First(hProcessShot, &ProcessInformation) && INVALID_HANDLE_VALUE!= hProcessShot) {
+		do
+		{
+			printf("PROCESS NAME:  %ls  -  ", ProcessInformation.szExeFile );
+			printf("PROCESS ID:  %i \n", ProcessInformation.th32ProcessID );
+		}
+		while (Process32Next(hProcessShot, &ProcessInformation));
+	}
+	else {
+
+	}
+	CloseHandle(hProcessShot);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
